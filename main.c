d0fedf46b5e137d4a485c005cb0a980c7f7a5340
janithchamika <janithchamika526@gmail.com>
2026-09-17 17:28:33 +0530

    Update Bill Print Service

--- main.c
+++ main.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <string.h>
-
-
-#define MAX_PATIENTS 100
-
-const char *wardNames[] = {"General Ward", "Paediatric Ward", "Surgical Ward", "ICU"};
-const int bedCapacities[] = {20, 10, 10, 5};
+#include "hospital.h"
 
 int patientCount = 0;
 int currentQueueCount[4] = {0}; //4 use for It has only 4 ques only
@@ -24,9 +17,12 @@ int main() {
 
         printf("\n======================================================\n");
         printf("        SMART HOSPITAL & RESOURCE ALLOCATION SYSTEM     \n");
-        printf("=======================================================\n");
-        printf("1. Register a New Patient\n");
-        printf("2. Exit\n");
+        printf("=======================================================\n\n");
+        printf("\t1. Register a New Patient\n");
+        printf("\t2. Print Patient Bill\n");
+        printf("\t3. View Priority List\n");
+        printf("\t4. View Analytics Reports\n");
+        printf("\t5. Exit\n\n");
         printf("Enter your choice: ");
         scanf("%hd", &choice);
 
@@ -89,13 +85,123 @@ int main() {
                 printf("\n ~ Patient Registered Successfully !!\n");
 
                 break;
+
+
             case 2:
+                printf("\n--- Print Patient Bill ---\n");
+
+                if (patientCount == 0) {
+                    printf("No patients registered yet!\n");
+                    break;
+                }
+
+                int searchID;
+                printf("Enter Patient ID (maximum 100): ");
+                scanf("%d", &searchID);
+
+                int index = searchID - 1;
+
+                if (index < 0 || index >= patientCount) {
+                    printf("Patient ID not found!\n");
+                    break;
+                }
+
+                int specIndex = specialtyID[index] - 1; // to know and cal  general, surgical bla bla bla
+
+                float baseFee = baseFees[specIndex];
+                float surcharge = 0.0;
+                int surchargePct = 0;
+                char urgencyStr[30];
+
+                if (urgencyLevel[index] == 1) {
+                    strcpy(urgencyStr, "Level 1 (Normal)");
+                } else if (urgencyLevel[index] == 2) {
+                    strcpy(urgencyStr, "Level 2 (Urgent)");
+                    surcharge = baseFee * 0.20;
+                    surchargePct = 20;
+                } else {
+                    strcpy(urgencyStr, "Level 3 (Critical)");
+                    surcharge = baseFee * 0.50;
+                    surchargePct = 50;
+                }
+
+                float wardCost = 0.0;
+                char wardStr[50];
+                if (isAdmitted[index] == 1) {
+                    int wIndex = wardID[index] - 1;
+                    wardCost = daysAdmitted[index] * wardRates[wIndex];
+                    sprintf(wardStr, "%s", wardNames[wIndex]);
+                } else {
+                    strcpy(wardStr, "Not Admitted (OPD)");
+                }
+
+                float grossTotal = baseFee + surcharge + wardCost;
+
+                float discount = 0.0;
+                char ageStr[50];
+                if (patientAges[index] < 5 || patientAges[index] > 65) {
+                    discount = grossTotal * 0.15;
+                    sprintf(ageStr, "%d Years (15%% Subsidy Eligible)", patientAges[index]);
+                } else {
+                    sprintf(ageStr, "%d Years (No Subsidy)", patientAges[index]);
+                }
+
+                float finalPayable = grossTotal - discount;
+
+                float waitTime = (currentQueueCount[specIndex] - 1) * consultationTimes[specIndex];
+                if (waitTime < 0) waitTime = 0.0;
+
+                const char* specNames[] = {"General Practice (OPD)", "Paediatrics", "Cardiology", "Neurology"};
+
+                printf("\n======================================================\n");
+                printf("            SMART HOSPITAL ADMISSION & BILL           \n");
+                printf("=======================================================\n");
+                printf("Patient ID           : PAT-%d\n", searchID);
+                printf("Patient Name         : %s\n", patientNames[index]);
+                printf("Age                  : %s\n", ageStr);
+                printf("Specialty            : %s\n", specNames[specIndex]);
+                printf("Assigned Ward        : %s\n", wardStr);
+                printf("Urgency Level        : %s\n", urgencyStr);
+                printf("Base Consultation Fee: LKR %.2f\n", baseFee);
+                printf("Emergency Surcharge  : LKR %.2f (%d%%)\n", surcharge, surchargePct);
+
+                if (isAdmitted[index] == 1) {
+                    printf("Ward Stay Cost       : LKR %.2f (%d Days)\n", wardCost, daysAdmitted[index]);
+                } else {
+                    printf("Ward Stay Cost       : LKR 0.00\n");
+                }
+
+                printf("Gross Total Bill     : LKR %.2f\n", grossTotal);
+
+                if (discount > 0) {
+                    printf("Age Subsidy Discount : LKR -%.2f (15%%)\n", discount);
+                } else {
+                    printf("Age Subsidy Discount : LKR 0.00\n");
+                }
+
+                printf("Final Payable Amount : LKR %.2f\n", finalPayable);
+
+                if (waitTime == 0) {
+                    printf("Estimated Wait Time  : 0.00 mins (Immediate Attention)\n");
+                } else {
+                    printf("Estimated Wait Time  : %.2f mins\n", waitTime);
+                }
+                printf("=======================================================\n");
+                break;
+
+            case 3:
+                printf("Exiting System. Good Bye!\n");
+                break;
+            case 4:
+                printf("Exiting System. Good Bye!\n");
+                break;
+            case 5:
                 printf("Exiting System. Good Bye!\n");
                 break;
             default:
                 printf("Invalid Choice! Please try again.\n");
         }
-    } while(choice != 2);
+    } while(choice != 5);
 
     return 0;
 }
