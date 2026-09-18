69b6618cad7bc0d5981b72770185a7ac605d9bca
janithchamika <janithchamika526@gmail.com>
2026-09-18 16:28:51 +0530

    Update header file to format the system

--- hospital.h
+++ hospital.h
@@ -6,6 +6,14 @@
 
 #define MAX_PATIENTS 100
 
+#define C_RED     "\033[0;31m"
+#define C_GREEN   "\033[0;32m"
+#define C_LGREEN  "\033[1;32m"
+#define C_YELLOW  "\033[0;33m"
+#define C_LBLUE   "\033[1;36m"
+#define C_ORANGE  "\033[38;5;208m"
+#define C_RESET   "\033[0m"
+
 const char *wardNames[] = {"General Ward", "Paediatric Ward", "Surgical Ward", "ICU"};
 const int bedCapacities[] = {20, 10, 10, 5};
 const float baseFees[] = {1500.0, 2500.0, 4500.0, 5000.0};
@@ -13,3 +21,4 @@ const int consultationTimes[] = {15, 20, 30, 30};
 const float wardRates[] = {3000.0, 6000.0, 12000.0, 25000.0};
 
 #endif
+
