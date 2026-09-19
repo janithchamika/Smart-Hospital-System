5130d9d5787a892c0c729be09902b4b49211df08
janithchamika <janithchamika526@gmail.com>
2026-09-19 11:56:23 +0530

    Updated header file to formate

--- hospital.h
+++ hospital.h
@@ -6,22 +6,19 @@
 
 #define MAX_PATIENTS 100
 
-#define C_RED     "\033[0;31m"
-#define C_GREEN   "\033[0;32m"
-#define C_LGREEN  "\033[1;32m"
+#define C_RED     "\033[38;5;196m"
+#define C_GREEN   "\033[38;5;46m"
 #define C_YELLOW  "\033[0;33m"
-#define C_LYELLOW "\033[93m"
 #define C_LBLUE   "\033[1;36m"
-#define C_ORANGE  "\033[38;5;208m"
+#define C_ORANGE  "\033[38;5;214m"
 #define C_RESET   "\033[0m"
 
 #define C_TITLE  "\033[1;34m"  
 #define C_MENU   "\033[1;37m"  
-#define C_PROMPT "\033[1;35m"
+#define C_PROMPT "\033[38;5;201m"
 
 #define T_BOLD      "\033[1m"
 #define T_ITALIC    "\033[3m"
-#define T_UNDERLINE "\033[4m"
 
 const char *wardNames[] = {"General Ward", "Paediatric Ward", "Surgical Ward", "ICU"};
 const int bedCapacities[] = {20, 10, 10, 5};
